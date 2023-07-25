ef7e2e5818422e59a9318528cf34b7d5f49ee9be
bhandarisaurav <saurav.bhandari012@gmail.com>
2023-07-25 14:20:55 -0400

    Moved BookListAPI inside BookListProject

--- BookListProject/BookListProject/urls.py
+++ BookListProject/BookListProject/urls.py
@@ -16,6 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+from BookListAPI import views
 
 urlpatterns = [path("admin/", admin.site.urls), path("books/", views.books)]
