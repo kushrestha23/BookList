30ebc1328deaa7e10b7d79592c0a6113fc1d1f62
kushrestha23 <kushrestha23@gmail.com>
2023-07-27 17:00:50 -0400

    Removed the render function

--- BookListAPI/views.py
+++ BookListAPI/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse, JsonResponse
 from .models import Book
 from django.views.decorators.csrf import csrf_exempt
 from django.forms.models import model_to_dict
-from django.shortcuts import render
 
 
 # Create your views here.
