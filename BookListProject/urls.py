dd54d507e253b7b2abe9526e30e2780c10a8201f
kushrestha23 <kushrestha23@gmail.com>
2023-08-01 10:21:50 -0400

    django toolbar integrated

--- BookListProject/urls.py
+++ BookListProject/urls.py
@@ -21,5 +21,6 @@ from BookListAPI import views
 urlpatterns = [
     path("", views.index, name='index'),
     path("admin/", admin.site.urls),
-    path('api/v1/',include('BookListAPI.urls'))
+    path('api/v1/',include('BookListAPI.urls')),
+    path('__debug__/',include('debug_toolbar.urls')),
 ]
